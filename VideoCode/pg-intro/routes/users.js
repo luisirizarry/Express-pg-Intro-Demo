04da56963735f22b29bd97247e22a5593d1f358e
Luis Irizarry <lirizarry6178@gmail.com>
2024-11-23 21:19:04 -0500

    start of demo

---
+++ VideoCode/pg-intro/routes/users.js
@@ -0,0 +1,9 @@
+/** Routes for users of pg-intro-demo. */
+const express = require("express");
+const ExpressError = require("../expressError")
+const router = express.Router();
+const db = require("../db");
+
+
+
+module.exports = router;
