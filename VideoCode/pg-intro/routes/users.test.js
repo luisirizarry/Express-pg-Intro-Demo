ac1806ea4c319eb7b5325d75262b5f90a4bf0ec5
Luis Irizarry <lirizarry6178@gmail.com>
2024-11-25 00:00:42 -0500

    finished demo

--- VideoCode/pg-intro/routes/users.test.js
+++ VideoCode/pg-intro/routes/users.test.js
@@ -6,68 +6,84 @@ const app = require('../app');
 const db = require('../db');
 
 let testUser;
+
 beforeEach(async () => {
-  const result = await db.query(`INSERT INTO users (name, type) VALUES ('Peanut', 'admin') RETURNING  id, name, type`);
-  testUser = result.rows[0]
-})
+  const result = await db.query(
+    `INSERT INTO users (name, type) VALUES ('Peanut', 'admin') RETURNING id, name, type`
+  );
+  testUser = result.rows[0];
+});
 
 afterEach(async () => {
-  await db.query(`DELETE FROM users`)
-})
+  await db.query(`DELETE FROM users`);
+});
 
 afterAll(async () => {
-  await db.end()
-})
+  await db.end();
+});
 
 describe("GET /users", () => {
   test("Get a list with one user", async () => {
-    const res = await request(app).get('/users')
+    const res = await request(app).get('/users');
     expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual({ users: [testUser] })
-  })
-})
+    expect(res.body).toEqual({ users: [testUser] });
+  });
+});
 
 describe("GET /users/:id", () => {
-  test("Gets a single user", async () => {
-    const res = await request(app).get(`/users/${testUser.id}`)
+  test("Get a user", async () => {
+    const res = await request(app).get(`/users/${testUser.id}`);
     expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual({ user: testUser })
-  })
-  test("Responds with 404 for invalid id", async () => {
-    const res = await request(app).get(`/users/0`)
+    expect(res.body).toEqual({ user: testUser });
+  });
+
+  test("Try to get an invalid user", async () => {
+    const res = await request(app).get(`/users/0`);
     expect(res.statusCode).toBe(404);
-  })
-})
+  });
+});
 
 describe("POST /users", () => {
   test("Creates a single user", async () => {
-    const res = await request(app).post('/users').send({ name: 'BillyBob', type: 'staff' });
+    const res = await request(app)
+      .post('/users')
+      .send({ name: 'Billybob', type: 'admin' });
     expect(res.statusCode).toBe(201);
     expect(res.body).toEqual({
-      user: { id: expect.any(Number), name: 'BillyBob', type: 'staff' }
-    })
-  })
-})
+      user: {
+        id: expect.any(Number),
+        name: 'Billybob',
+        type: 'admin',
+      },
+    });
+  });
+});
 
-describe("PATCH /users/:id", () => {
+describe("PATCH /users", () => {
   test("Updates a single user", async () => {
-    const res = await request(app).patch(`/users/${testUser.id}`).send({ name: 'BillyBob', type: 'admin' });
+    const res = await request(app)
+      .patch(`/users/${testUser.id}`)
+      .send({ name: 'Billybob', type: 'admin' });
     expect(res.statusCode).toBe(200);
     expect(res.body).toEqual({
-      user: { id: testUser.id, name: 'BillyBob', type: 'admin' }
-    })
-  })
-  test("Responds with 404 for invalid id", async () => {
-    const res = await request(app).patch(`/users/0`).send({ name: 'BillyBob', type: 'admin' });
-    expect(res.statusCode).toBe(404);
-  })
-})
-describe("DELETE /users/:id", () => {
+      user: {
+        id: testUser.id, // Use dynamic testUser.id
+        name: 'Billybob',
+        type: 'admin',
+      },
+    });
+  });
+});
+
+describe("DELETE /users", () => {
   test("Deletes a single user", async () => {
     const res = await request(app).delete(`/users/${testUser.id}`);
     expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual({ msg: 'DELETED!' })
-  })
-})
 
-
+    // Confirm the user was deleted
+    const check = await db.query(`SELECT * FROM users WHERE id = $1`, [
+      testUser.id,
+    ]);
+    expect(check.rows.length).toBe(0);
+  });
+});
